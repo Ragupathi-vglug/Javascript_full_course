d58cb629338929bfea3c806aca84b8d7644bd834
MADARA143390 <ragu26555@gmail.com>
2026-03-16 21:18:55 +0530

    Files added

---
+++ Basic_program/script.js
@@ -0,0 +1,15 @@
+console.log("Hello World");
+console.log(12345);
+console.log(true);
+console.log([1,2,3,4,"hi",true,null]);
+console.log({name  :"Ragupathi",age :21});
+console.log("custom");
+console.table({name  :"Ragupathi",age :21});
+console.error("First error");
+console.warn("It's a warning message");
+// console.clear();
+console.time("Starting time");
+for(i=0;i<10;i++){
+    console.log(i);
+}
+console.timeEnd("Ending time");
