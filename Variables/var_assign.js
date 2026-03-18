5e013d2cb2cd6f12cef9dfb75d79aae89a58818a
MADARA143390 <ragu26555@gmail.com>
2026-03-18 21:50:13 +0530

    files added

---
+++ Variables/var_assign.js
@@ -0,0 +1,15 @@
+console.log("Var type variable")
+var name="Ragupathi";
+console.log("Before the transition :",name);
+name="ragu";
+console.log("\nLet type variable")
+console.log("After the transition :",name);
+let age=21;
+console.log("Before the transition :",age);
+age=20;
+console.log("After the transition :",age);
+console.log("\nConst type variable")
+const gender="Male";
+console.log("Before the word change:",gender);
+gender="Boy";
+console.log(gender);
