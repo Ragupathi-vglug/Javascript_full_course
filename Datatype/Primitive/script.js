c1046f9c53a41b89459a2c03f457bc9f35669dbb
MADARA143390 <ragu26555@gmail.com>
2026-03-18 22:12:10 +0530

    files added

---
+++ Datatype/Primitive/script.js
@@ -0,0 +1,13 @@
+var a=25; //NUMBER
+var fname="Ragu";
+var isMarried=false;
+var phone =null;
+var b;
+const s1=Symbol();
+
+console.log(typeof(a));
+console.log(typeof(fname));
+console.log(typeof(isMarried));
+console.log(typeof(phone));
+console.log(typeof(b));
+console.log(s1);
