7002a839ad646f25f0be4f67309e8541e5fb25b7
MADARA143390 <ragu26555@gmail.com>
2026-03-22 21:28:21 +0530

    Files added

---
+++ Operators/Assignment Operator/script.js
@@ -0,0 +1,12 @@
+let a=10;
+console.log("A Equal :",a);
+a+=5;
+console.log("Add Equal :",a);
+a-=5
+console.log("Sub Equal:",a);
+a*=5;
+console.log("Mul Equal :",a);
+a/=5;
+console.log("Div Equal :",a);
+a%=5;
+console.log("Mod Equal :",a);
