fa7074aa4ea52967fbc8860573206445d0902a33
MADARA143390 <ragu26555@gmail.com>
2026-04-03 23:14:48 +0530

    Files added

---
+++ Operators/Relational operator/script.js
@@ -0,0 +1,8 @@
+a=10;
+b=20;
+console.log("A :",a);
+console.log("B :",b);
+console.log("A Greater than B:",a>b);
+console.log("A Lesser than B:",a<b);
+console.log("A Greater than or equal to B:",a>=b);
+console.log("A Lesser than or equal to B:",a<=b);
