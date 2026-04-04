9bf6f7175843dc3c7dcc8591a10afd9aa7642ed9
MADARA143390 <ragu26555@gmail.com>
2026-04-04 20:05:21 +0530

    New files added for the operators

---
+++ Operators/Condtional operaator/script.js
@@ -0,0 +1,3 @@
+const age=15;
+let result=age>=18?"Eligible":"Not eligible";
+console.log("Age :",age," is ",result);
---
+++ Operators/Handling null values/script.js
@@ -0,0 +1,9 @@
+// Using conditional operator
+function welcome(name){
+    let result=name?name:"No name";
+    console.log("Welcome :",result);
+}
+welcome("Ragu pathi");
+welcome();
+
+// Using Objects
