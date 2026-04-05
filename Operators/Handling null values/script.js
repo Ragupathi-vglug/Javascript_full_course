00ee00a5b7a47f3886f19875eb1ecd50020e9843
MADARA143390 <ragu26555@gmail.com>
2026-04-05 20:32:00 +0530

    New files added for the operators

--- Operators/Handling null values/script.js
+++ Operators/Handling null values/script.js
@@ -7,3 +7,17 @@ welcome("Ragu pathi");
 welcome();
 
 // Using Objects
+let user={name:"Ragu",age:21};
+console.log(user);
+console.log(user.name);
+const greet=(user)=>{
+    const name=user.name?user.name:"No name";
+    return "Hello"+name;
+}
+console.log(greet(user));
+
+
+// Conitional chain
+let avg=75;
+const res=avg>90?"A grade":avg>80?"B grade":"C grade";
+console.log(avg,"Has ",res);
