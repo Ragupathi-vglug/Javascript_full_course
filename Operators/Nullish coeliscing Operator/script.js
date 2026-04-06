12724c5a8e5621d6566ca84e856d5172b8533dea
MADARA143390 <ragu26555@gmail.com>
2026-04-06 21:45:29 +0530

    New files added for the operators

--- Operators/Nullish coeliscing Operator/script.js
+++ Operators/Nullish coeliscing Operator/script.js
@@ -1,5 +1,12 @@
+// Example 01
 const a=null??"No value";
 console.log(a);
 
 const b=null??45;
 console.log(b);
+
+// Example 02
+const user={name:"Ragu"};
+console.log(user);
+user.city??="Cuddalore";
+console.log(user);
