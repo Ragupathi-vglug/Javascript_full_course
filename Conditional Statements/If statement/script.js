f7d34cbee32601cd13a7d0064ba5fb46079729fd
MADARA143390 <ragu26555@gmail.com>
2026-04-07 18:52:25 +0530

    New files added for the operators

---
+++ Conditional Statements/If statement/script.js
@@ -0,0 +1,8 @@
+let age=prompt("Enter the age :");
+let fname="Ragupathi";
+if (age>=18){
+    alert(fname+" is Eligible for Voting");
+}
+else{
+    alert(fname+" is not eligible for the Voting.")
+}
