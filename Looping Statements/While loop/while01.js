20adb32018322a08b9c19dd24fc058ee8be1a56e
MADARA143390 <ragu26555@gmail.com>
2026-04-28 20:41:15 +0530

    looping added

--- Looping Statements/While loop/while01.js
+++ Looping Statements/While loop/while01.js
@@ -1,4 +1,14 @@
-let i=0;
-while(i<=10){
-    console.log(i);
+let j=0;
+while(j<=10){
+    console.log(j);
+}
+
+// Do while Loop
+
+let table=2,limit=5,i=1;
+do{
+    console.log(table," x ",i," = ",table*i);
+    i++;
+}
+while(i<=5){
 }
