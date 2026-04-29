94db1232708b981263330b95380b583d78176ed0
MADARA143390 <ragu26555@gmail.com>
2026-04-29 21:01:04 +0530

    looping added

--- Looping Statements/for loop/script.js
+++ Looping Statements/for loop/script.js
@@ -1,4 +1,25 @@
-let n=prompt("Enter the range :");
+// For loop
+// let n=prompt("Enter the range :");
+let n=5;
 for(i=1;i<=n;i++){
     console.log("*".repeat(i));
 }
+
+//Ex 02
+let arr=[];
+for(i=1;i<100;i++){
+    arr.push(i);
+}
+console.log(arr);
+console.log(arr[50]);
+
+// Nested for loop
+let nums=[];
+for(let i=0;i<3;i++){
+    nums.push([]);
+    for(let j=0;j<3;j++){
+        nums.push(j);
+    }
+}
+console.log(nums);
+// console.table(nums);
