cf44de39353da21764331e0e6534867121d73546
MADARA143390 <ragu26555@gmail.com>
2026-05-31 20:51:33 +0530

    Files added

---
+++ Functions/User_defined/script.js
@@ -0,0 +1,32 @@
+function add(a,b){
+    return a+b;
+}
+console.log(add(10,10));
+
+// Function with arbitary argument
+function sum(){
+    let total=0;
+    for(i=0;i<arguments.length;i++){
+        total+=arguments[i];
+    }
+    return total;
+}
+console.log(sum(1,2,3,4,5,6,7,8,9,0));
+
+// Spread Operator
+function mul(...args){
+    let total=1;
+    for(i=1;i<args.length;i++){
+        total*=args[i];
+        
+    }
+    return total;
+}
+console.log(mul(1,2,3,4,5,6,7,8,9));
+
+// Function as Expression
+const sub=function(a,b){
+    return a-b;
+}
+console.log(sub(10,5));
+
