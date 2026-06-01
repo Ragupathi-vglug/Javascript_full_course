a3605bebab8ff7fa8ebaf64b535f66b39f2bdd34
MADARA143390 <ragu26555@gmail.com>
2026-06-01 17:34:40 +0530

    Files added

--- Functions/Arrow_function/script.js
+++ Functions/Arrow_function/script.js
@@ -4,3 +4,10 @@ const add=(a,b)=>{
 console.log(add);
 console.log(add(10,20));
 
+// Using arrow function with map() 
+let nums=[1,2,3,4,5,6];
+const double_nums=nums.map(num=>num*2);
+console.log(nums);
+console.log(double_nums);
+
+// Using arrow function with filter()
