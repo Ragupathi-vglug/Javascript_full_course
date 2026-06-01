dd2344949ad81b5d97d86ca6aa5207683f90b3ec
MADARA143390 <ragu26555@gmail.com>
2026-06-01 21:26:18 +0530

    Files added

--- Functions/Arrow_function/script.js
+++ Functions/Arrow_function/script.js
@@ -11,3 +11,14 @@ console.log(nums);
 console.log(double_nums);
 
 // Using arrow function with filter()
+let fruits=["Apple","Mango","Orange","Grapes"];
+console.log(fruits);
+let fruit_fil=fruits.filter(fr=>fr.length>5);
+console.log(fruit_fil);
+
+// Using arrow function with reduce()
+let c=[1,2,3,4,5];
+console.log(c);
+let tot=c.reduce((sum,d)=>sum+d,0);
+console.log(tot);
+
