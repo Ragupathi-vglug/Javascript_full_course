85906f82e08811d023f78dadecae2bc7114791d5
MADARA143390 <ragu26555@gmail.com>
2026-06-02 21:27:26 +0530

    Files added

---
+++ Array/script.js
@@ -0,0 +1,11 @@
+// Method 1
+let student=[101,"Ragupathi",21];
+console.log(student);
+console.log(student[0]);
+console.log(student[1]);
+console.log(typeof(student));
+
+// Method 2
+let a=new Array(1,2,3,4);
+console.log(a);
+console.log(typeof(student));
