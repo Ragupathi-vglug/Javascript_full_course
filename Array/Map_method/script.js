9dfbe1a66bfbe1572b88b248154dded54c355e76
MADARA143390 <ragu26555@gmail.com>
2026-06-07 17:36:20 +0530

    Files added

---
+++ Array/Map_method/script.js
@@ -0,0 +1,27 @@
+const numbers=[1,2,3,4,5,6,7,8,9];
+let sqrt=numbers.map((value)=>{
+    return Math.sqrt(value).toFixed(2);
+})
+console.log(sqrt);
+
+console.log("\n");
+
+// In complex object map()
+let users=[
+    {name:"Ragu",age:21,city:"Panrutti"},
+    {name:"Jenny",age:20,city:"washington"}
+];
+console.log(users);
+let eleigible_user=users.map((user)=>({
+    /*
+    name:user.name,
+    age:user.age,
+    city:user.city,
+    */
+    ...user,
+    status:user.age>=18?"Eligible":"Not eligible"
+}));
+console.table(eleigible_user);
+
+// map() advance
+// 1) Simpple transformation
