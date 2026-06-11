7a0d8b86b8d17f15e46cff77dc12b6bbc95a0ffb
MADARA143390 <ragu26555@gmail.com>
2026-06-11 15:46:38 +0530

    Files added

--- Array/Reduce_method/script.js
+++ Array/Reduce_method/script.js
@@ -7,6 +7,7 @@ let numbers=[1,2,3,4,5,6];
 let sum=numbers.reduce((accumulator,current_value)=>accumulator+current_value);
 console.log(sum);
 
+console.log("\n");
 // Flattering an array
 let nest=[[1,2],[3,4]];
 let flattering=nest.reduce((ac,cur)=>ac.concat(cur));
@@ -27,4 +28,22 @@ let col_count=colors.reduce((ac,cur)=>{
 },{});
 console.log(colors);
 console.log(col_count);
+
+console.log("\n");
+// Ex 02
+let people=[
+    {name:"Ragupathi",age:21,city:"Panrutti"},
+    {name:"stella",age:19,city:"california"},
+    {name:"alpha",age:17,city:"california"}
+];
+let groupbycity=people.reduce((ac,cur)=>{
+    if(cur.city in ac){
+        ac[cur.city].push(cur);
+    }
+    else{
+        ac[cur.city]=[cur];
+    }
+    return ac;
+},{});
+console.log(groupbycity);
 
