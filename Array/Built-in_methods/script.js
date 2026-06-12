f0a71e070e906e0e561cfee857ebd243deecdbcd
MADARA143390 <ragu26555@gmail.com>
2026-06-12 19:17:40 +0530

    Files added

--- Array/Built-in_methods/script.js
+++ Array/Built-in_methods/script.js
@@ -5,3 +5,11 @@ console.log(numbers.slice(2));
 console.log(numbers.slice(2,5));
 
 // Splice() method
+console.log("\n");
+// Basic
+let n1=[1,2,3,4,5,6,7,8,9,0];
+console.log("Before splice :",n1);
+let rm_items=n1.splice(2);
+console.log("Removed items :",rm_items);
+console.log("After splice :",n1);
+
