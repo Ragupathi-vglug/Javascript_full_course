cae40e35ca06dc97e367962b09e7c45e23838285
MADARA143390 <ragu26555@gmail.com>
2026-06-19 22:31:32 +0530

    array other concepts added

--- Array/other_concepts/script.js
+++ Array/other_concepts/script.js
@@ -10,3 +10,7 @@ res=n.some((value)=>{
 console.log(res);
 
 // Primitive and Reference data type
+const obj1={a:1,b:2};
+const obj2={c:3,d:4};
+const obj3=Object.assign({},obj1,obj2);
+console.log(obj3);
