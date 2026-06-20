4ce2841ad42a90a7590fce89a7e35efe7580f193
MADARA143390 <ragu26555@gmail.com>
2026-06-20 21:49:40 +0530

    Files added

--- Object_type/diff_object/script.js
+++ Object_type/diff_object/script.js
@@ -1,6 +1,8 @@
 // Object literal notation
 const students={name:"Ragupathi",father:"Jayabalan",age:21,address:"panrutti",about:function(){
-return ${this.name},"is from",{$this.address};
+return ${this.name},"is from",${this.address};
 }};
 console.log(students);
 console.log(students.about());
+
+// Factory function
