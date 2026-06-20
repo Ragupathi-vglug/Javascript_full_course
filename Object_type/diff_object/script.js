684af6001010b27eafb598063df83e95ae9e4ba3
MADARA143390 <ragu26555@gmail.com>
2026-06-20 21:48:37 +0530

    Files added

---
+++ Object_type/diff_object/script.js
@@ -0,0 +1,6 @@
+// Object literal notation
+const students={name:"Ragupathi",father:"Jayabalan",age:21,address:"panrutti",about:function(){
+return ${this.name},"is from",{$this.address};
+}};
+console.log(students);
+console.log(students.about());
